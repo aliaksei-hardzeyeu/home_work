ffe65cafb9b77568d1b59766a873f3d1bfd024dc
AliakseiAH <fugent89@yandex.ru>
2021-04-08 20:29:14 +0300

    loops task2_4 final look, поменял местами вызовы методов shrinkArray и sumOfNumbers из-за особенностей реализации shrinkArray

--- src/home_work_2/arrays/Task2_4.java
+++ src/home_work_2/arrays/Task2_4.java
@@ -4,24 +4,16 @@ import java.util.Arrays;
 
 import static home_work_2.arrays.ArraysUtils.arrayRandom;
 
-//2.4 Задачи взяты с сайта https://taskcode.ru/array. На сайте есть пояснения по каждой из этих задач.
-// Все задачи в одном классе, в отдельных методах. Для создания массивов использовать ранее созданный arrayRandom.
-//		2.4.1. Сумма четных положительных элементов массива
-//		2.4.2. Максимальный из элементов массива с четными индексами
-//		2.4.3. Элементы массива, которые меньше среднего арифметического
-//		2.4.4. Найти два наименьших (минимальных) элемента массива
-//		2.4.5. Сжать массив, удалив элементы, принадлежащие интервалу
-//		2.4.6. Сумма цифр массива
+
 public class Task2_4 {
     public static void main(String[] args) {
-        //int[] container = arrayRandom(10, 100);
-        int[] container = {22, 11, -10, 99, 87, -13, -27};
-        //evenPositiveSum(container);
-        //maxEvenIndexElement(container);
-        //lessThanAverage(container);
-       // twoMinElements(container);
-        //shrinkArray(container, 0,30);
+        int[] container = arrayRandom(10, 100);
+        evenPositiveSum(container);
+        maxEvenIndexElement(container);
+        lessThanAverage(container);
+        twoMinElements(container);
         sumOfNumbers(container);
+        shrinkArray(container, 0,30);
     }
 
     public static void evenPositiveSum(int[] container) {
@@ -75,10 +67,10 @@ public class Task2_4 {
     }
 
     public static void twoMinElements(int[] container) {
-        System.out.println();
+        System.out.println("\n");
         System.out.println(Arrays.toString(container));
         System.out.println("Найти два наименьших (минимальных) элемента массива");
-        int temp = 0;
+        int temp;
         for (int i = 0; i < container.length; i++) {
             for (int j = 1; j < container.length - i; j++) {
                 if (container[j-1] >= container[j]) {
@@ -94,7 +86,7 @@ public class Task2_4 {
     public static void shrinkArray (int[] container, int lowerRange, int upperRange) {
         System.out.println();
         System.out.println(Arrays.toString(container));
-        System.out.println("Сжать массив, удалив элементы, принадлежащие интервалу");
+        System.out.println("Сжать массив, удалив элементы, принадлежащие интервалу от " + lowerRange + " до " + upperRange);
         for (int i = 0; i < container.length; i++)
             if (container[i] <= upperRange && container[i] >= lowerRange) container[i] = 0;
 
@@ -120,8 +112,7 @@ public class Task2_4 {
         System.out.println(Arrays.toString(container));
         System.out.println("Сумма цифр массива");
     int sum = 0;
-    int remainder;
-        for (int i = 0; i < container.length;i++) {
+    for (int i = 0; i < container.length;i++) {
             int elementSum;
             int n = Math.abs(container[i]);
             for (elementSum = 0; n!=0; n = n/10) {
--- src/home_work_2/loops/Test.java
+++ src/home_work_2/loops/Test.java
@@ -1,18 +1,53 @@
 package home_work_2.loops;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-        //int[] container = {1, -2, 10, 4, 5, -6, 11, 9};
-        int n = 158;
-            int    sum;
-
-        for(sum=0 ;n!=0 ;n/=10)
-        {
-            sum+=n%10;
-        }
-        System.out.println("Sum of digits of a number is "+sum);
+     int[] container = {22, 11, -10, 99, 87, -13, -27};
+     shrinkArray(container, 10, 60);
+     sumOfNumbers(container);
     }
-       }
+
+    public static void shrinkArray (int[] container, int lowerRange, int upperRange) {
+        System.out.println();
+        System.out.println(Arrays.toString(container));
+        System.out.println("Сжать массив, удалив элементы, принадлежащие интервалу от " + lowerRange + " до " + upperRange);
+        for (int i = 0; i < container.length; i++)
+            if (container[i] <= upperRange && container[i] >= lowerRange) container[i] = 0;
+
+        int newLength = 0;
+        for (int element:container) {
+            if (element != 0) newLength++;
+        }
+
+        int[] newContainer = new int[newLength];
+
+        int count = 0;
+        for (int element:container) {
+            if (element != 0) {
+                newContainer[count] = element;
+                count++;
+            }
+        }
+        System.out.println(Arrays.toString(newContainer));
+    }
+    public static void sumOfNumbers (int[] container) {
+        System.out.println();
+        System.out.println(Arrays.toString(container));
+        System.out.println("Сумма цифр массива");
+        int sum = 0;
+        for (int i = 0; i < container.length;i++) {
+            int elementSum;
+            int n = Math.abs(container[i]);
+            for (elementSum = 0; n!=0; n = n/10) {
+                elementSum += n % 10;
+            }
+            sum = sum + elementSum;
+        }
+        System.out.println(sum);
+    }
+}
 
 
 
