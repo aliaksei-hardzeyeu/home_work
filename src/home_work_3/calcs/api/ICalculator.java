87aa44fccc0e5388e9d44c2189da72758e5cfdf4
AliakseiAH <fugent89@yandex.ru>
2021-04-13 21:21:08 +0300

    Task8

---
+++ src/home_work_3/calcs/api/ICalculator.java
@@ -0,0 +1,13 @@
+package home_work_3.calcs.api;
+
+public interface ICalculator {
+
+    double division(double x, double y);
+    double multiplying(double x, double y);
+    double subtraction(double x, double y);
+    double addition(double x, double y);
+    double power(double x, int y);
+    double absolute(double x);
+    double squareRoot(double x);
+
+    }
