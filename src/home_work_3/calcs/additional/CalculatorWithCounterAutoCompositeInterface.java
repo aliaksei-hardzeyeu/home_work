7ac1a127d4a2a6d09ec5cde7bfa8f41ba176a208
AliakseiAH <fugent89@yandex.ru>
2021-04-20 15:02:34 +0300

    Task 9

--- src/home_work_3/calcs/additional/CalculatorWithCounterAutoCompositeInterface.java
+++ src/home_work_3/calcs/additional/CalculatorWithCounterAutoCompositeInterface.java
@@ -11,7 +11,90 @@ public class CalculatorWithCounterAutoCompositeInterface {
     }
 
     long countOperation;
+    public void incrementCountOperation(){ countOperation++;}
     public long getCountOperation() {
         return countOperation;
+    }
+
+    /**
+     * Деление
+     *
+     * @param x делимое
+     * @param y делитель
+     * @return частное
+     */
+    public double division(double x, double y) {
+        incrementCountOperation();
+        return calculator.division(x, y);
+    }
+
+    /**
+     * Умножение
+     *
+     * @param x первый множитель
+     * @param y второй множитель
+     * @return произведение
+     */
+    public double multiplying(double x, double y) {
+        incrementCountOperation();
+        return calculator.multiplying(x, y);
+    }
+
+    /**
+     * Вычитание
+     *
+     * @param x уменьшаемое
+     * @param y вычитаемое
+     * @return разность
+     */
+    public double subtraction(double x, double y) {
+        incrementCountOperation();
+        return calculator.subtraction(x, y);
+    }
+
+    /**
+     * Сложение
+     *
+     * @param x первое слагаемое
+     * @param y второе слагаемое
+     * @return сумма
+     */
+    public double addition(double x, double y) {
+        incrementCountOperation();
+        return calculator.addition(x, y);
+    }
+
+    /**
+     * Возведение в степень
+     *
+     * @param x возводимое число
+     * @param y степень (по условию положительная и целая)
+     * @return произведение
+     */
+    public double power(double x, int y) {
+        incrementCountOperation();
+        return calculator.power(x, y);
+    }
+
+    /**
+     * Модуль числа
+     *
+     * @param x число
+     * @return его модуль
+     */
+    public double absolute(double x) {
+        incrementCountOperation();
+        return calculator.absolute(x);
+    }
+
+    /**
+     * Квадратный корень числа
+     *
+     * @param x число
+     * @return корень
+     */
+    public double squareRoot(double x) {
+        incrementCountOperation();
+        return calculator.squareRoot(x);
     }
 }
--- src/home_work_3/runners/CalculatorWithCounterAutoCompositeInterfaceMain.java
+++ src/home_work_3/runners/CalculatorWithCounterAutoCompositeInterfaceMain.java
@@ -2,9 +2,23 @@ package home_work_3.runners;
 
 import home_work_3.calcs.additional.CalculatorWithCounterAutoCompositeInterface;
 import home_work_3.calcs.simple.CalculatorWithMathCopy;
+import home_work_3.calcs.simple.CalculatorWithMathExtends;
+import home_work_3.calcs.simple.CalculatorWithOperator;
 
 public class CalculatorWithCounterAutoCompositeInterfaceMain {
     public static void main(String[] args) {
-       // CalculatorWithCounterAutoCompositeInterface calculator = new CalculatorWithCounterAutoCompositeInterface (CalculatorWithMathCopy x);
+        CalculatorWithCounterAutoCompositeInterface calculator1 = new CalculatorWithCounterAutoCompositeInterface(new CalculatorWithMathCopy());
+        System.out.println(calculator1.addition((calculator1.addition((calculator1.power((calculator1.division(28, 5)), 2)), 4.1)), (calculator1.multiplying(15, 7))));
+        System.out.println("Количество вызовов " + calculator1.getCountOperation());
+
+
+        CalculatorWithCounterAutoCompositeInterface calculator2 = new CalculatorWithCounterAutoCompositeInterface(new CalculatorWithMathExtends());
+        System.out.println(calculator2.addition((calculator2.addition((calculator2.power((calculator2.division(28, 5)), 2)), 4.1)), (calculator2.multiplying(15, 7))));
+        System.out.println("Количество вызовов " + calculator2.getCountOperation());
+
+
+        CalculatorWithCounterAutoCompositeInterface calculator3 = new CalculatorWithCounterAutoCompositeInterface(new CalculatorWithOperator());
+        System.out.println(calculator3.addition((calculator3.addition((calculator3.power((calculator3.division(28, 5)), 2)), 4.1)), (calculator3.multiplying(15, 7))));
+        System.out.println("Количество вызовов " + calculator3.getCountOperation());
     }
 }
