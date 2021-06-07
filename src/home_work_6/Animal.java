48eb440edd2e84da3d1d4aaeeef6701b6212aa4f
Alex <fugent89@yandex.ru>
2021-06-07 21:39:44 +0300

    home_work_6 задания 1 - 4 черновой вариант, полный комплекс методов тоько для ArrayList

--- src/home_work_6/Animal.java
+++ src/home_work_6/Animal.java
@@ -3,19 +3,10 @@ package home_work_6;
 import java.util.Collection;
 import java.util.Random;
 
-public class Animal {
+public class Animal extends Creature {
 
     private int age;
     private String nick;
-
-    public Animal(int age, String nick) {
-        this.age = age;
-        this.nick = nick;
-    }
-
-    public Animal() {
-
-    }
 
     public int getAge() {
         return age;
@@ -39,51 +30,5 @@ public class Animal {
                 "age=" + age +
                 ", nick='" + nick + '\'' +
                 '}' + '\n';
-    }
-
-    /**
-     * Генерирует объект Animal, задаёт age и nick
-     * @return объект Animal
-     */
-    public static Animal animalGenerator() {
-
-        Animal animal = new Animal();
-        int ageSize = (int) (Math.random() * 15) + 1;  ????????ОСТАНОВИЛСЯ ЗДЕСЬ!!!! ПЕРЕДЕЛАТЬ ИЗ ПЁРСОНА В ЭНИМАЛ
-        char[] password = new char[passwordSize];
-        Random random = new Random();
-        String setOfCharactersForPassword = "1234567890qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
-
-        for (int i = 0; i < passwordSize; i++) {
-            int randomInt = random.nextInt(setOfCharactersForPassword.length());
-            char randomChar = setOfCharactersForPassword.charAt(randomInt);
-            password[i] = randomChar;
-        }
-        person.setPassword(new String(password));
-
-        String setOfCharactersForNick = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
-        int nickSize = (int) (Math.random() * 10) + 4;
-        char[] nick = new char[nickSize];
-
-        for (int i = 0; i < nickSize; i++) {
-            int randomInt = random.nextInt(setOfCharactersForNick.length());
-            char randomChar = setOfCharactersForNick.charAt(randomInt);
-            nick[i] = randomChar;
-        }
-        person.setNick(new String(nick));
-
-        return person;
-    }
-
-    /**
-     * Метод заполняет переданный тип коллекции заданным количеством объектов Person
-     * @param collection тип коллекции
-     * @param number количество объектов
-     * @return возвращает заполненную коллекцию
-     */
-    public static Collection<Person> fillCollection (Collection<Person> collection, int number) {
-        for (int i = 0; i < number; i++) {
-            collection.add(personGenerator());
-        }
-        return collection;
     }
 }
--- src/home_work_6/MainApp.java
+++ src/home_work_6/MainApp.java
@@ -3,15 +3,130 @@ package home_work_6;
 import java.util.*;
 
 public class MainApp {
-    public static void main(String[] args) {
-        PersonPasswordComparator comp1 = new PersonPasswordComparator();
-        PersonNickComparator comp2 = new PersonNickComparator();
-//
-//        System.out.println(Person.fillCollection(new ArrayList<>(), 1_0) + "\n**********");
-//        System.out.println(Person.fillCollection(new LinkedList<>(), 1_0) + "\n**********");
-//        System.out.println(Person.fillCollection(new HashSet<>(), 1_0) + "\n**********");
-        System.out.println(Person.fillCollection(new TreeSet<>(comp1.thenComparing(comp2)), 1_0) + "\n**********");
 
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        ArrayList arrayList = (ArrayList) MainApp.fillCollection(new ArrayList<>(), 1_000_000, "Person");
+        long finish = System.currentTimeMillis();
+        System.out.println("ArrayList filling took " + (finish - start));
+
+        start = System.currentTimeMillis();
+        arrayList.sort(new PersonPasswordComparator().thenComparing(new PersonNickComparator()));
+        finish = System.currentTimeMillis();
+        System.out.println("ArrayList sorting by 2 comparators took " + (finish - start));
+
+        Iterator<Person> iter = arrayList.iterator();
+        start = System.currentTimeMillis();
+        while (iter.hasNext()) {
+               iter.next();
         }
+        finish = System.currentTimeMillis();
+        System.out.println("ArrayList iterating by iterator all elements took " + (finish - start));
+
+        start = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            arrayList.get(i);
+        }
+        finish = System.currentTimeMillis();
+        System.out.println("ArrayList iterating by for_i all elements took " + (finish - start));
+
+        start = System.currentTimeMillis();
+        while (iter.hasNext()) {
+            iter.next();
+            iter.remove();
+        }
+        finish = System.currentTimeMillis();
+        System.out.println("ArrayList iterator removing all elements took " + (finish - start));
+
+//        start = System.currentTimeMillis();
+//        for (int i = 999999; i >= 0; i--) {
+//            arrayList.remove(i);
+//        }
+//        finish = System.currentTimeMillis();
+//        System.out.println("ArrayList for_i removing all elements took " + (finish - start));
+
     }
+
+
+    /**
+     * @param collection тип коллекции
+     * @param number     количество объектов
+     * @param type       тип объекта
+     * @return возвращает заполненную коллекцию
+     */
+    static Collection<?> fillCollection(Collection<? super Creature> collection, int number, String type) {
+        if (type.equals("Person")) {
+            for (int i = 0; i < number; i++) {
+                collection.add(personGenerator());
+            }
+        } else if (type.equals("Animal")) {
+            for (int i = 0; i < number; i++) {
+                collection.add(animalGenerator());
+            }
+        }
+        return collection;
+    }
+
+    /**
+     * Генерирует объект Person, задаёт password и nick
+     *
+     * @return объект Person
+     */
+    static Person personGenerator() {
+
+        Person person = new Person();
+        int passwordSize = (int) (Math.random() * 5) + 5;
+        char[] password = new char[passwordSize];
+        Random random = new Random();
+        String setOfCharactersForPassword = "1234567890qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
+
+        for (int i = 0; i < passwordSize; i++) {
+            int randomInt = random.nextInt(setOfCharactersForPassword.length());
+            char randomChar = setOfCharactersForPassword.charAt(randomInt);
+            password[i] = randomChar;
+        }
+        person.setPassword(new String(password));
+
+        String setOfCharactersForNick = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
+        int nickSize = (int) (Math.random() * 10) + 4;
+        char[] nick = new char[nickSize];
+
+        for (int i = 0; i < nickSize; i++) {
+            int randomInt = random.nextInt(setOfCharactersForNick.length());
+            char randomChar = setOfCharactersForNick.charAt(randomInt);
+            nick[i] = randomChar;
+        }
+        person.setNick(new String(nick));
+
+        return person;
+    }
+
+    /**
+     * Генерирует объект Animal, задаёт age и nick
+     *
+     * @return объект Animal
+     */
+    static Animal animalGenerator() {
+        Animal animal = new Animal();
+
+        int age = (int) (Math.random() * 14) + 1;
+        animal.setAge(age);
+
+        Random random = new Random();
+        String setOfCharactersForNick = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
+        int nickSize = (int) (Math.random() * 10) + 4;
+        char[] nick = new char[nickSize];
+
+        for (int i = 0; i < nickSize; i++) {
+            int randomInt = random.nextInt(setOfCharactersForNick.length());
+            char randomChar = setOfCharactersForNick.charAt(randomInt);
+            nick[i] = randomChar;
+        }
+        animal.setNick(new String(nick));
+
+        return animal;
+    }
+
+}
+
 
