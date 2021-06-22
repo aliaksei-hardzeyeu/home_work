af68198568d53d33b8d2eff09385b2ff0f9a3d84
Alex <fugent89@yandex.ru>
2021-06-22 10:16:12 +0300

    home_work_7 задание 7 дополнил

--- src/home_work_7/IOMain.java
+++ src/home_work_7/IOMain.java
@@ -5,14 +5,13 @@ import home_work_2.sorts.SortUtils;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
 
 public class IOMain {
     public static void main(String[] args) throws IOException {
-        chooseBook();
+        chooseBookSearchWrite();
     }
 
-    public static void chooseBook() throws IOException {
+    public static void chooseBookSearchWrite() throws IOException {
 
         InputStreamReader reader = new InputStreamReader(System.in);
         BufferedReader bufferedReader = new BufferedReader(reader);
@@ -28,40 +27,52 @@ public class IOMain {
             System.out.println(book);
         }
 
-        System.out.println("**********************************\n Выберите книгу");
-        Path chosenFile = Path.of(directory.getAbsolutePath() + bufferedReader.readLine());
+        boolean anotherBook = false;
+        do  {
 
-        String chosenBook = Files.readString(chosenFile);
+            System.out.println("**********************************\n Выберите книгу");
+            Path chosenFile = Path.of(directory.getAbsolutePath() + bufferedReader.readLine());
 
-        boolean anotherSearch = false;
-        do {
+            String chosenBook = Files.readString(chosenFile);
 
-            System.out.println("**********************************\n Введите слово для поиска");
-            String searchedWord = bufferedReader.readLine();
+            boolean anotherSearch = false;
+            do {
 
-            ISearchEngine searchEngine = new RegExSearch();
-            long counter = searchEngine.search(chosenBook, searchedWord);
+                System.out.println("**********************************\n Введите слово для поиска");
+                String searchedWord = bufferedReader.readLine();
 
-            System.out.println(searchedWord + " " + counter);
+                ISearchEngine searchEngine = new RegExSearch();
+                long counter = searchEngine.search(chosenBook, searchedWord);
 
-            try {
-                BufferedWriter writer = new BufferedWriter(new FileWriter(directory.getAbsoluteFile() + "test.txt", true));
-                writer.write("Книга: " + chosenFile + " >>> слово: " + searchedWord + " >>> встречается " + counter + " раз\n");
-                writer.flush();
+                System.out.println(searchedWord + " " + counter);
 
-            } catch (IOException e) {
-                System.out.println("Ошибка при работе с файлом");
-            }
+                try {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter(directory.getAbsoluteFile() + "result.txt", true));
+                    writer.write("Книга: " + chosenFile + " >>> слово: " + searchedWord + " >>> встречается " + counter + " раз\n");
+                    writer.flush();
 
-            System.out.println("Будем искать ещё слова в этой книге? 1 - да, 2 - нет");
-            String answer = bufferedReader.readLine();
+                } catch (IOException e) {
+                    System.out.println("Ошибка при работе с файлом");
+                }
 
-            if (answer.equals("1")) {
-                anotherSearch = true;
-            } else {
-                return;
-            }
+                System.out.println("Будем искать ещё слова в этой книге? 1 - да, 2 - нет");
+                String answer1 = bufferedReader.readLine();
 
-        } while (anotherSearch);
+                if (answer1.equals("1")) {
+                    anotherSearch = true;
+                } else if (answer1.equals("2")) {
+                    System.out.println("Для выбора другой книги введите 1. Для выхода нажмите 2.");
+                    String answer2 = bufferedReader.readLine();
+
+                    if (answer2.equals("1")) {
+                        anotherBook = true;
+                    } else {
+                        return;
+                    }
+                }
+
+            } while (anotherSearch);
+
+        } while (anotherBook);
     }
 }
